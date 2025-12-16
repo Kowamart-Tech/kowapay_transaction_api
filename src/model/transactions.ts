d08002dc0f7602bc95e034ad4ca13de4ea7b9ea3
shazaniyu55 <shazaniyu@gmail.com>
2025-12-16 15:53:37 +0100

    'server'

---
+++ src/model/transactions.ts
@@ -0,0 +1,48 @@
+import pool from "../config/db.js";
+
+export interface ITransaction {
+  id: string; // UUID
+  user: string; // UUID
+  amount: number;
+  currency: string;
+  status: "pending" | "completed" | "failed";
+  paymentMethod: string;
+  paymentType?: string;
+  transactionId?: string;
+  usedForPayment: boolean;
+  reference: string;
+  metadata?: Record<string, any>;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const Transactions = {
+
+    async  createTransaction(tx: ITransaction) {
+        const query = `
+            INSERT INTO transactions 
+            (user, amount, currency, status, payment_method, payment_type, transaction_id, used_for_payment, reference, metadata)
+            VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)
+            RETURNING *;
+        `;
+  
+  const values = [
+    tx.user,
+    tx.amount,
+    tx.currency,
+    tx.status,
+    tx.paymentMethod,
+    tx.paymentType || null,
+    tx.transactionId || "",
+    tx.usedForPayment,
+    tx.reference,
+    tx.metadata || null,
+  ];
+
+  const result = await pool.query(query, values);
+  return result.rows[0];
+}
+
+}
+
+export default Transactions;
